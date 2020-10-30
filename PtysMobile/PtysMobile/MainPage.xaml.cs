0a2fec65e5cd2e37cd4dcfe01635cd8d8e532b16
MB <mariuszlodz@gmail.com>
2020-10-30 16:21:39 +0100

    add page

--- PtysMobile/PtysMobile/MainPage.xaml.cs
+++ PtysMobile/PtysMobile/MainPage.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace PtysMobile
 {
@@ -13,6 +15,35 @@ namespace PtysMobile
         public MainPage()
         {
             InitializeComponent();
+            DebugClass();
+        }
+
+        //wszystkie obrazy osadzone
+        public void DebugClass()
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            }
+        }
+    }
+
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
         }
     }
 }
