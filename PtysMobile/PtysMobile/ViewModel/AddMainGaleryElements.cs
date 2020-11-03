91a0277907220edc539febf03b7814217de18536
MB <mariuszlodz@gmail.com>
2020-11-03 12:06:44 +0100

    change model of aplication

---
+++ PtysMobile/PtysMobile/ViewModel/AddMainGaleryElements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PtysMobile.ViewModel
+{
+    public class AddMainGaleryElements
+    {
+        public AddMainGaleryElements()
+        {
+
+        }
+
+        string torty = "Torty";
+
+        public String Torty
+        {
+            get { return torty; }
+        }
+    }
+}
--- PtysMobile/PtysMobile/pageMainGalery.xaml.cs
+++ PtysMobile/PtysMobile/pageMainGalery.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PtysMobile.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace PtysMobile
         public pageMainGalery()
         {
             InitializeComponent();
+            BindingContext = new AddMainGaleryElements();
         }
 
         #region buttons
